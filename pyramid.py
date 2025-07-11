85eb6fd7f44f0947e218680847f74bf14467b962
guhan-1283 <144509433+guhan-1283@users.noreply.github.com>
2025-07-11 09:51:46 +0530

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,7 +1,16 @@
+# for odd number of strings
 n = int(input("Enter Number of rows: "))
 for i in range(1,n+1):
     for j in range(n-i):
         print(" ",end=" ")
     for k in range(1,2*i):
         print('*',end=" ")
+    print()
+
+# for even number of strings
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i):
+        print('* ',end="")
     print()
